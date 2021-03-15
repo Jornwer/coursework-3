439e5ecc28d4a969567a9c10dfae5492d7b033de
Jornwer <betanovdmitry946@gmail.com>
2021-03-15 13:14:34 +0300

    added security on server

---
+++ server/src/main/java/com/jornwer/coursework/model/Status.java
@@ -0,0 +1,5 @@
+package com.jornwer.coursework.model;
+
+public enum Status {
+    ACTIVE, BANNED
+}
---
+++ server/src/main/java/com/jornwer/coursework/model/User.java
@@ -0,0 +1,24 @@
+package com.jornwer.coursework.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+}
